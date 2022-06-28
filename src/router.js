f711cdeb2950d5d33366fd7d22c0faae742bc30b
Quentin Hiribarne <39909159+salatiiga@users.noreply.github.com>
2022-06-28 18:17:28 +0200

    add name to route

--- src/router.js
+++ src/router.js
@@ -1,19 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      component: () => import("../src/views/HomePage.vue"),
-    },
-    {
-      path: "/register",
-      component: () => import("../src/views/RegisterPage.vue"),
-    },
-    {
-      path: "/sign-in",
-      component: () => import("../src/views/SignInPage.vue"),
-    },
-  ],
+    history: createWebHistory(),
+    routes: [
+        {
+            name: "homePage",
+            path: "/",
+            component: () => import("../src/views/HomePage.vue"),
+        },
+        {
+            name: "registerPage",
+            path: "/register",
+            component: () => import("../src/views/RegisterPage.vue"),
+        },
+        {
+            name: "signInPage",
+            path: "/sign-in",
+            component: () => import("../src/views/SignInPage.vue"),
+        },
+    ],
 });
 export default router;
