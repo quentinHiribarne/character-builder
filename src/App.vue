fb20a6f4f312eaa7bfe278497d3f2d6c6cf8573e
Quentin Hiribarne <39909159+salatiiga@users.noreply.github.com>
2022-06-28 14:48:37 +0200

    auth example

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,63 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld :msg="message" />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { ref } from "vue";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD1X7FDQ66jFw2stfe6Cx-UCDoxtAwAjzI",
+  authDomain: "character-builder-33d77.firebaseapp.com",
+  databaseURL:
+    "https://character-builder-33d77-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "character-builder-33d77",
+  storageBucket: "character-builder-33d77.appspot.com",
+  messagingSenderId: "434435830572",
+  appId: "1:434435830572:web:bb7fc76939605dccf3f289",
+  measurementId: "G-LFM4SCRHLT",
+};
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+  },
+  setup() {
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+
+    let email = "quentin.hiribarne@gmail.com";
+    let password = "quentin";
+    let message = ref("");
+
+    // const user = ref({});
+    const errorCode = ref("");
+    const errorMessage = ref("");
+
+    // Initialize Firebase Authentication and get a reference to the service
+    const auth = getAuth();
+    const authenticate = () => {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          message.value = user.uid;
+          console.log(user);
+        })
+        .catch((error) => {
+          errorCode.value = error.code;
+          errorMessage.value = error.message;
+        });
+    };
+
+    authenticate();
+    return {
+      message,
+    };
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,26 +2,4 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 createApp(App).mount("#app");
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD1X7FDQ66jFw2stfe6Cx-UCDoxtAwAjzI",
-  authDomain: "character-builder-33d77.firebaseapp.com",
-  projectId: "character-builder-33d77",
-  storageBucket: "character-builder-33d77.appspot.com",
-  messagingSenderId: "434435830572",
-  appId: "1:434435830572:web:bb7fc76939605dccf3f289",
-  measurementId: "G-LFM4SCRHLT",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
