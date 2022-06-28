1914a6ca14d35dd2231eaadd8503897075940e9f
Quentin Hiribarne <39909159+salatiiga@users.noreply.github.com>
2022-06-28 15:06:29 +0200

    fix firebase conf emplacement

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,8 @@
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
-import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ref } from "vue";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD1X7FDQ66jFw2stfe6Cx-UCDoxtAwAjzI",
-  authDomain: "character-builder-33d77.firebaseapp.com",
-  databaseURL:
-    "https://character-builder-33d77-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "character-builder-33d77",
-  storageBucket: "character-builder-33d77.appspot.com",
-  messagingSenderId: "434435830572",
-  appId: "1:434435830572:web:bb7fc76939605dccf3f289",
-  measurementId: "G-LFM4SCRHLT",
-};
 
 export default {
   name: "App",
@@ -27,9 +14,6 @@ export default {
     HelloWorld,
   },
   setup() {
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-
     let email = "quentin.hiribarne@gmail.com";
     let password = "quentin";
     let message = ref("");
