23765055c633b32d341602a7ae3452e9399cd181
Quentin Hiribarne <39909159+salatiiga@users.noreply.github.com>
2022-06-28 18:08:55 +0200

    register/login form + serviceworker

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,28 @@
 <template>
-  <div>
-    <nav>
-      <router-link to="/"> Home </router-link> |
-      <router-link to="/register"> Register </router-link> |
-      <router-link to="/sign-in"> Login </router-link> |
-    </nav>
-    <router-view />
-  </div>
+    <div>
+        <nav>
+            <router-link to="/"> Home </router-link> |
+            <router-link to="/register"> Register </router-link> |
+            <router-link to="/sign-in"> Login </router-link> |
+        </nav>
+        <router-view />
+    </div>
 </template>
 
 <script>
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { ref } from "vue";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD1X7FDQ66jFw2stfe6Cx-UCDoxtAwAjzI",
-  authDomain: "character-builder-33d77.firebaseapp.com",
-  databaseURL:
-    "https://character-builder-33d77-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "character-builder-33d77",
-  storageBucket: "character-builder-33d77.appspot.com",
-  messagingSenderId: "434435830572",
-  appId: "1:434435830572:web:bb7fc76939605dccf3f289",
-  measurementId: "G-LFM4SCRHLT",
-};
-
 export default {
-  name: "App",
-  components: {},
-  setup() {
-    // Initialize Firebase
-    const app = initializeApp(firebaseConfig);
-
-    let email = "quentin.hiribarne@gmail.com";
-    let password = "quentin";
-    let message = ref("");
-
-    // const user = ref({});
-    const errorCode = ref("");
-    const errorMessage = ref("");
-
-    // Initialize Firebase Authentication and get a reference to the service
-    const auth = getAuth();
-    const authenticate = () => {
-      signInWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          // Signed in
-          const user = userCredential.user;
-          message.value = user.uid;
-        })
-        .catch((error) => {
-          errorCode.value = error.code;
-          errorMessage.value = error.message;
-        });
-    };
-
-    authenticate();
-    return {
-      message,
-    };
-  },
+    name: "App",
+    components: {},
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -21,23 +21,25 @@
 //   },
 // });
 module.exports = {
-  transpileDependencies: true,
-  devServer: {
-    https: true,
-  },
-  pwa: {
-    name: "Character Builder",
-    themeColor: "#4DBA87",
-    msTileColor: "#000000",
-    appleMobileWebAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "black",
+    transpileDependencies: true,
+    devServer: {
+        https: false,
+        allowedHosts: "all",
+        host: "0.0.0.0",
+    },
+    pwa: {
+        name: "Character Builder",
+        themeColor: "#4DBA87",
+        msTileColor: "#000000",
+        appleMobileWebAppCapable: "yes",
+        appleMobileWebAppStatusBarStyle: "black",
 
-    // // configure the workbox plugin
-    // workboxPluginMode: "InjectManifest",
-    // workboxOptions: {
-    //   // swSrc is required in InjectManifest mode.
-    //   swSrc: "./src/registerServiceWorker.js",
-    //   // ...other Workbox options...
-    // },
-  },
+        // // configure the workbox plugin
+        // workboxPluginMode: "InjectManifest",
+        // workboxOptions: {
+        //   // swSrc is required in InjectManifest mode.
+        //   swSrc: "./src/registerServiceWorker.js",
+        //   // ...other Workbox options...
+        // },
+    },
 };
