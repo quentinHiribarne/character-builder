b479f74f64000361d084d3d7f29fa4940337b1ce
Quentin Hiribarne <39909159+salatiiga@users.noreply.github.com>
2022-07-04 17:57:38 +0200

    add logout

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,53 @@
     <div>
         <nav>
             <router-link to="/"> Home </router-link> |
-            <router-link to="/register"> Register </router-link> |
-            <router-link to="/sign-in"> Login </router-link> |
+            <span v-if="isLoggedIn">
+                <button @click="logout">Logout</button>
+            </span>
+            <span v-else>
+                <router-link to="/register"> Register </router-link> |
+                <router-link to="/sign-in"> Login </router-link>
+            </span>
         </nav>
         <router-view />
     </div>
 </template>
 
 <script>
+import { ref } from "vue"; // used for conditional rendering
+import { getAuth, signOut, onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "vue-router";
+
 export default {
     name: "App",
     components: {},
+    setup() {
+        const router = useRouter();
+        const isLoggedIn = ref(true);
+
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                isLoggedIn.value = true; // if we have a user
+            } else {
+                isLoggedIn.value = false; // if we do not
+            }
+        });
+
+        const logout = () => {
+            signOut(auth)
+                .then(() => {
+                    router.push("/sign-in");
+                })
+                .catch((error) => {
+                    console.log(error.code, error.message);
+                });
+        };
+        return {
+            logout,
+            isLoggedIn,
+        };
+    },
 };
 </script>
 
